2db5d01967636f324219c7e1913051533a11949b
Abdoulaye HAIDARA <abdoulaye.haidara@edu.univ-paris13.fr>
2025-09-28 16:00:00 +0200

    modification *.scala

--- immutabilite/Plan.scala
+++ immutabilite/Plan.scala
@@ -1,14 +1,15 @@
 object Plan  {
+    def replanifie(plan: List[String], nouvelleVille: String, avantVille: String): List[String] = {
+        val index = plan.indexOf(avantVille)
+        val villeAvant = plan.slice(0, index)
+        val villeApres = plan.slice(index, plan.size)
+        villeAvant.appended(nouvelleVille).appendedAll(villeApres)    
+    } 
+    
+    
+    
     def main(args: Array[String]): Unit = {
-       def replanifie(plan: List[String], nouvelleVille: String, avantVille: String): List[String] = {
-            val index = plan.indexOf(avantVille)
-            val villeAvant = plan.slice(0, index)
-            val villeApres = plan.slice(index, plan.size)
-            villeAvant.appended(nouvelleVille).appendedAll(villeApres)
-
-            
-        } 
-
+       
         val plan1 = List("Paris", "Nantes")
         println("Plan 1 " + plan1)
 
